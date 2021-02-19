07f16579e5a89f8ed4d7bc5b31f8ccb81f73666b
yasha <yashach2005@gmail.com>
2021-02-19 20:47:25 +0200

    hw.3.py

---
+++ hw.3.py
@@ -0,0 +1,20 @@
+my_file = open("123.txt", "r")
+sum1 = []
+for s in my_file:
+    number = list(map(int, s.split()))
+    fizz = number[0]
+    buzz = number[1]
+    c = number[2]
+    for i in range(1 , c + 1):
+        if i % fizz == 0 and i % buzz == 0:
+            sum1.append("FB")
+        elif i % fizz == 0:
+            sum1.append("fizz")
+        elif i % buzz == 0:
+            sum1.append("buzz")
+        if i % fizz and i % buzz:
+            sum1.append(i)
+my_file.close()
+p = open("f.txt", "a")
+p.write(str(sum1))
+p.close()
